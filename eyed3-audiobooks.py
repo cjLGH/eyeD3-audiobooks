b51349cbbe1c2aa84b2bb41a12f504aef6b6b2d4
cjLGH <cjleonard@gmail.com>
2018-05-21 00:22:47 -0500

    Update eyed3-audiobooks.py

--- eyed3-audiobooks.py
+++ eyed3-audiobooks.py
@@ -2,19 +2,39 @@
 import readline
 import eyed3
 
+artist = ''
+while not artist:
+  artist = raw_input("artist: ")
+  artist = str(artist)
+  artist = unicode(artist, "utf-8")
+
+album = ''
+while not album:
+  album = raw_input("album: ")
+  album = str(album)
+  album = unicode(album, "utf-8")
+
+album_artist = ''
+while not album_artist:
+  album_artist = raw_input("album_artist: ")
+  album_artist = str(album_artist)
+  album_artist = unicode(album_artist, "utf-8")
+
 title = ''
 while not title:
   title = raw_input("Title: ")
   title = str(title)
   title = unicode(title, "utf-8")
 
-print(title)
-
-genre = raw_input("Genre: ")
+genre = ''
+while not genre:
+  genre = raw_input("genre: ")
 genre = int(genre)
-# genre = unicode(genre, "utf-8")
 
 audiofile = eyed3.load("1.mp3")
-audiofile.tag.title = title
-audiofile.tag.genre = genre
+audiofile.tag.title         = title
+audiofile.tag.album         = album
+audiofile.tag.album_artist  = album_artist
+audiofile.tag.title         = title
+audiofile.tag.genre         = genre
 audiofile.tag.save()
