46b7f33f00291d405c4f94ba8e0515ab75d50570
cjLGH <cjleonard@gmail.com>
2018-05-27 23:33:21 -0500

    Update eyed3util.py

--- eyed3util.py
+++ eyed3util.py
@@ -10,17 +10,7 @@ import os
 import signal
 import sys
 
-def sig_handler(signum=None, frame=None):
-    if signum is not None:
-        print('Signal %d caught, saving and exiting...' % signum)
-        shutdown()
-
-# Register signals, such as CTRL + C
-signal.signal(signal.SIGINT, sig_handler)
-signal.signal(signal.SIGTERM, sig_handler)
-
-def shutdown():
-    sys.exit(0)
+import main
 
 def which(program):
     import os
