e910af3553a305a049d9808ebbc79cec04dae29d
cjLGH <cjleonard@gmail.com>
2018-05-27 23:36:24 -0500

    Update eyed3util.py

--- eyed3util.py
+++ eyed3util.py
@@ -12,23 +12,6 @@ import sys
 
 import main
 
-def which(program):
-    import os
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
 def get_artist():
     while True:
         artist = raw_input("artist: ")
