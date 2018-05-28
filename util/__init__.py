821239edf7d061d1608e42b64410fa1f9fdeb35e
cjLGH <cjleonard@gmail.com>
2018-05-27 23:58:57 -0500

    Update __init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -1,15 +1,10 @@
 import os
-import signal
 import sys
 
 def sig_handler(signum=None, frame=None):
     if signum is not None:
-        print('Signal %d caught, saving and exiting...' % signum)
+        print('Signal %d caught, exiting...' % signum)
         shutdown()
-
-# Register signals, such as CTRL + C
-signal.signal(signal.SIGINT, sig_handler)
-signal.signal(signal.SIGTERM, sig_handler)
 
 def which(program):
     import os
