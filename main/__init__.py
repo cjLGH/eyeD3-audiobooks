edaa18c44b8202699513f91183d4bbd7d7ffa41b
cjLGH <cjleonard@gmail.com>
2018-05-27 23:36:09 -0500

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,4 @@
+import os
 import signal
 import sys
 
@@ -9,6 +10,23 @@ def sig_handler(signum=None, frame=None):
 # Register signals, such as CTRL + C
 signal.signal(signal.SIGINT, sig_handler)
 signal.signal(signal.SIGTERM, sig_handler)
+
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 def shutdown():
     sys.exit(0)
